813e465f2d5f55b703d68eff5374a31e3013f602
Scot <bscot.hagen@gmail.com>
2021-08-09 15:05:38 -0700

    added data annotations for validations and schema changes

--- RazorPagesMovie24/Migrations/20210809214142_New_DataAnnotations.Designer.cs
+++ RazorPagesMovie24/Migrations/20210809214142_New_DataAnnotations.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesMovie24.Data;
 
 namespace RazorPagesMovie24.Migrations
 {
     [DbContext(typeof(RazorPagesMovie24Context))]
-    partial class RazorPagesMovie24ContextModelSnapshot : ModelSnapshot
+    [Migration("20210809214142_New_DataAnnotations")]
+    partial class New_DataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- RazorPagesMovie24/Program.cs
+++ RazorPagesMovie24/Program.cs
@@ -1,13 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using RazorPagesMovie24.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace RazorPagesMovie24
 {
